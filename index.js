be4f081d490235267aab86eab6839178bbcdfd26
Pruthvi Shrikaanth <shrikaanthpruthvispareemail@gmail.com>
2020-12-26 16:58:51 +0000

    Reply feature improved

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const Discord = require("discord.js");
 const config = require("./config.js");
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 const token = config.token;
 const client = new Discord.Client();
 client.login(token);
 
 client.on("message",function(message) {
+    if (message.author.bot) return;
     if(message.channel.type=='dm') {
         const userID = message.author.id;
         const user = client.users.cache.get(userID);
-        user.send(message.content);
+        fromName = message.author.username;
+        content = message.content;
+        console.log(fromName,':',content);
+        readline.question('Message to reply:\n',messageToSend => {
+            user.send(messageToSend);
+            readline.close();
+        });
     }
 });
